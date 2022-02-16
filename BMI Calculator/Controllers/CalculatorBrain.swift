7abba59c370108c3ac0de6adb30284892933c8da
ElchinNasirov <nasirov.elchin@gmail.com>
2022-02-16 15:06:40 -0800

    Added BMI struct. Completed the app

--- BMI Calculator/Controllers/CalculatorBrain.swift
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -6,17 +6,33 @@
 //  Copyright © 2022 Angela Yu. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct CalculatorBrain {
-    var bmi: Float = 0.0
+    var bmi: BMI?
     
     mutating func calculateBMI(height: Float, weight: Float) {
-        bmi = weight / pow(height, 2)
+        let bmiValue = weight / pow(height, 2)
+        
+        if bmiValue < 19.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more!", color: UIColor.blue )
+        } else if bmiValue < 24.9 {
+            bmi = BMI(value: weight, advice: "Fit.", color: UIColor.green)
+        } else {
+            bmi = BMI(value: weight, advice: "Eat less!", color: UIColor.red)
+        }
     }
     
     func getBMIValue() -> String {
-        let bmiTo1DecimalPlace = String(format: "%.1f", bmi)
+        let bmiTo1DecimalPlace = String(format: "%.1f", bmi?.value ?? 0.0)
         return bmiTo1DecimalPlace
+    }
+    
+    func getAdvice() -> String {
+        return bmi?.advice ?? "No advice!"
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? UIColor.white
     }
 }
